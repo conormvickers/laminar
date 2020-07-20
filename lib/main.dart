a57ebbb5fb4912dc82abae6ed4decf8d550e6a8b
Conor Vickers <conormvickers@gmail.com>
2020-07-19 22:02:44 -0400

    redo with working tabs, not working yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'Navbar/Navbar.dart';
-import 'LandingPage/LandingPage.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+var myCont = TabController(
+  length: 3,
+  vsync: _MyHomePageState(),
+);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -31,14 +34,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
@@ -51,19 +47,289 @@ class _MyHomePageState extends State<MyHomePage> {
               end: Alignment.topRight,
               colors: [Colors.indigoAccent, Colors.lightBlueAccent]),
         ),
-        child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Navbar(),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 40.0),
-                child: LandingPage(),
-              )
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 40.0),
+                  child: LandingPage(),
+                ),
+              ),
+
             ],
-          ),
         ),
       ),
+    );
+  }
+}
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return DesktopNavbar();
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+          return DesktopNavbar();
+        } else {
+          return MobileNavbar();
+        }
+      },
+    );
+  }
+}
+
+class DesktopNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+                child: Row(
+                  children: [
+                    Text(
+                      "L",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30),
+                    ),
+                    Icon(
+                      MaterialCommunityIcons.lambda,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "M I N",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30),
+                    ),
+                    Icon(
+                      MaterialCommunityIcons.lambda,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "R  Medical",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30),
+                    ),
+                  ],
+                )),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  child: TabBar(
+                    controller: myCont,
+                    tabs: [
+                      Tab(
+                        child: Text('home'),
+                      ),
+                      Tab(child: Text('about'),),
+                      Tab(child: Text('contact')),
+                    ],
+                  ),
+                ),
+
+                SizedBox(
+                  width: 30,
+                ),
+                MaterialButton(
+                  color: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  onPressed: () {},
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MobileNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        child: Column(children: <Widget>[
+          Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "L",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30),
+                  ),
+                  Icon(
+                    MaterialCommunityIcons.lambda,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "M I N",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30),
+                  ),
+                  Icon(
+                    MaterialCommunityIcons.lambda,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "R  Medical",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30),
+                  ),
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "About Us",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "Apps",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  List<Widget> pageChildren(double width) {
+    return <Widget>[
+      Container(
+        width: width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Software that\njust works",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Text(
+                "We bring the latest and greatest technology to the medical field so that the healthcare community can stop fighting against clunky software systems and focus on fighting disease.",
+                style: TextStyle(fontSize: 16.0, color: Colors.white),
+              ),
+            ),
+            MaterialButton(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 40.0),
+                child: Text(
+                  "Our Packages",
+                  style: TextStyle(color: Colors.amber),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        child: Image.asset(
+          "minLogo.jpg",
+          width: width,
+        ),
+      ),
+//      TabBarView(
+//        controller: myCont,
+//        children: [
+//          Container(),
+//          Container(),
+//          Container()
+//        ],
+//      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    var setup1 = LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: pageChildren(constraints.biggest.width / 2),
+          );
+        } else {
+          return Column(
+            children: pageChildren(constraints.biggest.width),
+          );
+        }
+      },
+    );
+
+    return
+    Container(
+
+      child: Column(
+        children: [
+          Text('asdf') ,
+
+
+        ],
+      ),
+
+
+
     );
   }
 }
